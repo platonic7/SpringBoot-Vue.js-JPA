e0b2a99203207fdbc10b6d3d2e278d67b2514ed4
Platonic <jinseo84@gmail.com>
2018-10-11 00:13:10 +0900

    Add #getAllPosts()

--- src/main/java/com/platonic/springboot_vuejs_blog/controllers/BlogController.java
+++ src/main/java/com/platonic/springboot_vuejs_blog/controllers/BlogController.java
@@ -1,13 +1,26 @@
 package com.platonic.springboot_vuejs_blog.controllers;
 
+import com.platonic.springboot_vuejs_blog.entities.Post;
+import com.platonic.springboot_vuejs_blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BlogController {
 
+    @Autowired
+    private PostService postService;
+
     @GetMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "/posts")
+    public List<Post> posts() {
+        return postService.getAllPosts();
     }
 }
