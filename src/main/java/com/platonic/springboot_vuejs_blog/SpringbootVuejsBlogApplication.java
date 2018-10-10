630b04eeb08063638c2befe6617626633935e872
Platonic <jinseo84@gmail.com>
2018-10-11 00:00:50 +0900

    Create SpringBootApplication and Test.

---
+++ src/main/java/com/platonic/springboot_vuejs_blog/SpringbootVuejsBlogApplication.java
@@ -0,0 +1,12 @@
+package com.platonic.springboot_vuejs_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVuejsBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVuejsBlogApplication.class, args);
+    }
+}
